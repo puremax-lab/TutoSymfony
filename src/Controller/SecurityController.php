cf9ae0b86cf265540aa837c0b561dfce245e858a
puremax-lab <youtamaxime@gmail.com>
2025-07-11 14:20:40 +0200

    update

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,6 +12,10 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        if ($this->getUser()) {
+            $this->addFlash('error', 'You are already logged in.');
+            return $this->redirectToRoute('home');
+        }
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
